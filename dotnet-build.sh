fa1a020a7e0dcea35955a70c0fc7344ae4705323
Mastermindzh <info@rickvanlieshout.com>
2019-09-20 09:18:34 +0200

    polished up script

--- dotnet-build.sh
+++ dotnet-build.sh
@@ -3,18 +3,23 @@
 set -e
 
 # go to the workdir
-cd /api || return
+if [ -n "$BITBUCKET_CLONE_DIR" ]
+then
+    cd "$BITBUCKET_CLONE_DIR" || return
+else
+    cd /api || return
+fi
 
 # Add nuget source if access token is set
 if [ -n "$MYGET_ACCESS_TOKEN" ]
 then
-    echo "Adding inforit nuget source"
+    echo "Adding private myget source"
     SOURCE="https://www.myget.org/F/inforit/auth/$MYGET_ACCESS_TOKEN/api/v3/index.json"
     VAR=$(sed "/<\/packageSources>/i <add key=\"inforit.org\" value=\"$SOURCE\" protocolVersion=\"3\" />" ~/.nuget/NuGet/NuGet.Config)
     echo "$VAR" > ~/.nuget/NuGet/NuGet.Config
 fi
 
-CS_PROJECT_FILE="*.sln"
+CS_PROJECT_FILE=""
 CS_PROJECT_NAME="Api"
 DIST="./dist"
 RELEASE="Release"
@@ -42,15 +47,19 @@ rm -rf "$DIST"
 mkdir -p "$ARTIFACTS_DIST"
 
 # clean dependencies
+echo "cleaning $CS_PROJECT_FILE"
 dotnet clean $CS_PROJECT_FILE
 
 # restore dependencies
+echo "restoring $CS_PROJECT_FILE"
 dotnet restore $CS_PROJECT_FILE
 
 # build project
+echo "building $CS_PROJECT_FILE"
 dotnet build -c $RELEASE --no-restore $CS_PROJECT_FILE
 
 # run tests
+echo "running tests"
 for f in *.Test/*.csproj
 do
   echo "Processing $f file..."
@@ -58,10 +67,13 @@ do
 done
 
 # publish
+echo "publishing $CS_PROJECT_FILE"
 dotnet publish -c $RELEASE -o "$PROJECT_DIST" $CS_PROJECT_FILE
 
 # copy deployment files over
+echo "copying deployment files $CS_PROJECT_FILE"
 cp -r deployment/ $PROJECT_DIST
 
 # generate artifacts dir
+echo "generating artifacts"
 zip -r "$ARTIFACTS_DIST/$CS_PROJECT_NAME.zip" "$PROJECT_DIST"
