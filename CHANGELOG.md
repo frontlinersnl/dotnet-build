8d3a9b527cefe240d309738791b09f3dbe6f6676
Mastermindzh <info@rickvanlieshout.com>
2022-07-21 14:23:30 +0200

    - build file now matches test projects without case sensitivity - Nuget config only updated when required - Escaped simple params - Updated editorconfig to 2 spaces per tab - made deployment folder optional

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,48 +5,56 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [6.1.0]
+
+- build file now matches test projects without case sensitivity
+- Nuget config only updated when required
+- Escaped simple params
+- Updated editorconfig to 2 spaces per tab
+- made deployment folder optional
+
 ## [6.0.4]
 
--   Add nodejs and npm
+- Add nodejs and npm
 
 ## [6.0.3]
 
--   Dependabot SDK update
+- Dependabot SDK update
 
 ## [6.0.2]
 
--   Dependabot SDK update
+- Dependabot SDK update
 
 ## [6.0.1]
 
--   Dependabot SDK update
+- Dependabot SDK update
 
 ## [6.0.0]
 
--   Updated dotnet core sdk container to NET6
+- Updated dotnet core sdk container to NET6
 
 ## [5.3.1]
 
--   Updated dotnet core sdk container
+- Updated dotnet core sdk container
 
 ## [5.3.0]
 
--   Due to an issue with NuGet restore and certificates, a temporary version based on [NuGet Restore Issues](https://github.com/NuGet/Announcements/issues/49)
+- Due to an issue with NuGet restore and certificates, a temporary version based on [NuGet Restore Issues](https://github.com/NuGet/Announcements/issues/49)
 
 ## [5.2.0]
 
--   Add sonarscanner
+- Add sonarscanner
 
 ## [5.1.0]
 
--   Add codecoverage
+- Add codecoverage
 
 ## [5.0.0]
 
--   Changed base container to dotnet 5.0
+- Changed base container to dotnet 5.0
 
 ## [4.2.0]
 
 ### added
 
--   default-jre
+- default-jre
--- dotnet-build.sh
+++ dotnet-build.sh
@@ -5,24 +5,25 @@ set -e
 # enable nullglob - allows filename patterns which match no files to expand to a null string, rather than themselves
 shopt -s nullglob
 
-ROOTDIRECTORY="/api"
+ROOTDIRECTORY="${ROOTDIRECTORY:-"/api"}"
 
 # go to the workdir
-if [ -n "$BITBUCKET_CLONE_DIR" ]
-then
-    cd "$BITBUCKET_CLONE_DIR" || return
-    ROOTDIRECTORY="$BITBUCKET_CLONE_DIR"
+if [ -n "$BITBUCKET_CLONE_DIR" ]; then
+  cd "$BITBUCKET_CLONE_DIR" || return
+  ROOTDIRECTORY="$BITBUCKET_CLONE_DIR"
 else
-    cd /api || return
+  cd "$ROOTDIRECTORY" || return
 fi
 
 # Add nuget source if access token is set
-if [ -n "$MYGET_ACCESS_TOKEN" ]
-then
-    echo "Adding private myget source"
-    SOURCE="https://www.myget.org/F/inforit/auth/$MYGET_ACCESS_TOKEN/api/v3/index.json"
-    VAR=$(sed "/<\/packageSources>/i <add key=\"inforit.org\" value=\"$SOURCE\" protocolVersion=\"3\" />" ~/.nuget/NuGet/NuGet.Config)
-    echo "$VAR" > ~/.nuget/NuGet/NuGet.Config
+if [ -n "$MYGET_ACCESS_TOKEN" ]; then
+  NUGET_CONFIG="~/.nuget/NuGet/NuGet.Config"
+  echo "Adding private myget source"
+  SOURCE="https://www.myget.org/F/inforit/auth/$MYGET_ACCESS_TOKEN/api/v3/index.json"
+  if ! grep -q "$SOURCE" "$NUGET_CONFIG"; then
+    VAR=$(sed "/<\/packageSources>/i <add key=\"Inforit MyGet package source\" value=\"$SOURCE\" protocolVersion=\"3\" />" ~/.nuget/NuGet/NuGet.Config)
+    echo "$VAR" >~/.nuget/NuGet/NuGet.Config
+  fi
 fi
 
 CS_PROJECT_FILE="Api/Api.csproj"
@@ -31,19 +32,17 @@ DIST="./dist"
 RELEASE="Release"
 
 # change project name from default "Api"
-if [ -n "$PROJECT_NAME" ]
-then
+if [ -n "$PROJECT_NAME" ]; then
   CS_PROJECT_NAME="$PROJECT_NAME"
 fi
 
 # change project file location from default "Api.csproj" if specified
-if [ -n "$PROJECT_FILE" ]
-then
+if [ -n "$PROJECT_FILE" ]; then
   CS_PROJECT_FILE="$PROJECT_FILE"
 fi
 
 PROJECT_DIST="$DIST/$CS_PROJECT_NAME"
-ARTIFACTS_DIST="$DIST/artifacts"``
+ARTIFACTS_DIST="$DIST/artifacts"
 
 rm -rf ./**/bin
 rm -rf ./**/obj
@@ -54,37 +53,38 @@ mkdir -p "$ARTIFACTS_DIST"
 
 # clean dependencies
 echo "cleaning $CS_PROJECT_FILE"
-dotnet clean $CS_PROJECT_FILE
+dotnet clean "$CS_PROJECT_FILE"
 
 # restore dependencies
 echo "restoring $CS_PROJECT_FILE"
-dotnet restore $CS_PROJECT_FILE
+dotnet restore "$CS_PROJECT_FILE"
 
 # build project
 echo "building $CS_PROJECT_FILE"
-dotnet build -c $RELEASE --no-restore $CS_PROJECT_FILE
+dotnet build -c $RELEASE --no-restore "$CS_PROJECT_FILE"
 
 # run tests
+shopt -s nocaseglob # disable casing
 echo "running tests"
-for f in *.Test/*.csproj
-do
+for f in *.test/*.csproj; do
   echo "Processing $f file..."
-  dotnet test "$f" -c $RELEASE -r "$DIST/test-results" /p:CollectCoverage=true /p:CoverletOutputFormat="opencover" /p:CoverletOutput="$DIST/test-coverlet" 
+  dotnet test "$f" -c $RELEASE -r "$DIST/test-results" /p:CollectCoverage=true /p:CoverletOutputFormat="opencover" /p:CoverletOutput="$DIST/test-coverlet"
 done
+shopt -u nocaseglob # enable casing
 
 # collect codecoverage files and generate report for sonarcloud (warning is for backwards compatability)
 reportgenerator -reports:"**/*.opencover*.xml" -targetdir:"$DIST/coverage" -reporttypes:"Cobertura;HTMLInline;HTMLChart;SonarQube" || echo -e "\033[33mWARNING: codecoverage not succesfull\033[0m"
 
 # publish
 echo "publishing $CS_PROJECT_FILE"
-dotnet publish -c $RELEASE -o "$PROJECT_DIST" $CS_PROJECT_FILE
+dotnet publish -c $RELEASE -o "$PROJECT_DIST" "$CS_PROJECT_FILE"
 
 # copy deployment files over
-echo "copying deployment files $CS_PROJECT_FILE"
-cp -r deployment/ $PROJECT_DIST
+if [ -d "deployment/" ]; then
+  echo "copying deployment files $CS_PROJECT_FILE"
+  cp -r deployment/ "$PROJECT_DIST"
+fi
 
 # generate artifacts dir
 echo "generating artifacts"
-cd "$PROJECT_DIST"
-
-zip -r "$ROOTDIRECTORY/$ARTIFACTS_DIST/$CS_PROJECT_NAME.zip" "."
+zip -r "$ARTIFACTS_DIST/$CS_PROJECT_NAME.zip" "$PROJECT_DIST/"
