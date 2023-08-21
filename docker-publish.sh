db4e6c588c996e21ccd72f37cf51daf0d664214c
Mastermindzh <info@rickvanlieshout.com>
2023-08-21 09:18:55 +0200

    restoring correct tags after demonstrating builds

--- docker-publish.sh
+++ docker-publish.sh
@@ -2,9 +2,8 @@
 DOCKER_SCOPE="${DOCKER_SCOPE:-"inforitnl"}"
 
 TAGS=(
-  # "latest"
-  # "$(cat package.json | grep version | head -1 | awk -F: '{ print $2}' | sed 's/[\",]//g' | tr -d '[:space:]')"
-  "drone-test"
+  "latest"
+  "$(cat package.json | grep version | head -1 | awk -F: '{ print $2}' | sed 's/[\",]//g' | tr -d '[:space:]')"
 )
 
 NAME=$(cat package.json | grep name | head -1 | awk -F: '{ print $2}' | sed 's/[\",]//g' | tr -d '[:space:]')
