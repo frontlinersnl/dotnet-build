0330b5145dc5f6eddb5edb2180876643c4a49c8f
Andreea Cristina Strachinescu <andreea-inforit@netrom.ro>
2023-12-05 11:54:19 +0200

    build: rename inforit to frontliners

--- dotnet-build.sh
+++ dotnet-build.sh
@@ -19,9 +19,9 @@ fi
 if [ -n "$MYGET_ACCESS_TOKEN" ]; then
   NUGET_CONFIG="~/.nuget/NuGet/NuGet.Config"
   echo "Adding private myget source"
-  SOURCE="https://www.myget.org/F/inforit/auth/$MYGET_ACCESS_TOKEN/api/v3/index.json"
+  SOURCE="https://www.myget.org/F/frontliners/auth/$MYGET_ACCESS_TOKEN/api/v3/index.json"
   if ! grep -q "$SOURCE" "$NUGET_CONFIG"; then
-    VAR=$(sed "/<\/packageSources>/i <add key=\"Inforit MyGet package source\" value=\"$SOURCE\" protocolVersion=\"3\" />" ~/.nuget/NuGet/NuGet.Config)
+    VAR=$(sed "/<\/packageSources>/i <add key=\"Frontliners MyGet package source\" value=\"$SOURCE\" protocolVersion=\"3\" />" ~/.nuget/NuGet/NuGet.Config)
     echo "$VAR" >~/.nuget/NuGet/NuGet.Config
   fi
 fi
